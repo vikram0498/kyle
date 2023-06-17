09550309525fccef56f87d350ada45e431840e9d
vikram0498 <vikramsinghh.his@gmail.com>
2023-06-17 12:26:51 +0530

    add frontend code

--- backend/resources/views/auth/admin/login.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('auth.admin.layout')
-@section('title','Login')
-
-@section('content')
-
-    @livewire('auth.admin.login')
-
-@stop
--- backend/resources/views/auth/admin/reset-password.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('auth.admin.layout')
-@section('title',trans('global.reset_password'))
-
-@section('content')
-
-    @livewire('auth.admin.reset-password',['token'=>$token,'email'=>$email])
-
-@stop
