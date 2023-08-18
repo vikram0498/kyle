b9e901ef615b5dde7eab95ee45294dade905b7ab
amitpandey24 <amitpandey.his@gmail.com>
2023-08-18 11:50:09 +0530

    payment plans listing

--- src/component/pages/AdditionalCreadits.js
+++ src/component/pages/AdditionalCreadits.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import Header from "../partials/Layouts/Header";
 import Footer from "../partials/Layouts/Footer";
+import {useAuth} from "../../hooks/useAuth"; 
+import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+import axios from 'axios';
 
  const AdditionalCreadits = () => {
+    const [plans,setPlans] = useState([]);
+    const [isLoader, setIsLoader] = useState(true);
+    const [radioValue, setRadioValue] = useState('');
+    const {getTokenData} = useAuth();
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    useEffect(()=>{
+        getPlans();
+    },[]);
+
+    const getPlans = async ()=>{
+        try{
+            setIsLoader(true);
+            let headers = {
+                "Accept": "application/json", 
+                'Authorization': 'Bearer ' + getTokenData().access_token,
+                'auth-token' : getTokenData().access_token,
+            }
+
+            let plans = await axios.get(`${apiUrl}getAddtionalCredits`, { headers: headers });
+            setPlans(plans.data.data.additional_credits);
+            setIsLoader(false);
+        }catch(error){
+            if(error.response) {
+                if (error.response.data.errors) {
+                    toast.error(error.response.data.errors, {position: toast.POSITION.TOP_RIGHT});
+                }
+                if (error.response.data.error) {
+                    toast.error(error.response.data.error, {position: toast.POSITION.TOP_RIGHT});
+                }
+            }
+        }
+    }
     return(
         <>
+            <Header/>
             <section className="main-section position-relative pt-4 pb-5">
                 <div className="container position-relative">
                     <div className="back-block">
                         <div className="row">
                             <div className="col-12 col-sm-4 col-md-4 col-lg-4">
-                                <a href="#" className="back">
+                                <Link to="/" className="back">
                                     <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M15 6H1" stroke="#0A2540" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                                         <path d="M5.9 11L1 6L5.9 1" stroke="#0A2540" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                                     </svg>
                                     Back
-                                </a>
+                                </Link>
                             </div>
                             <div className="col-12 col-sm-4 col-md-4 col-lg-4">
                                 <h6 className="center-head text-center mb-0">Pricing Page</h6>
@@ -35,50 +73,22 @@ import Footer from "../partials/Layouts/Footer";
                                     <div className="card-box-light">
                                         <form>
                                             <div className="row">
-                                                <div className="col-12 col-md-6 col-lg-6">
-                                                    <div className="radio-item-features">
-                                                        <label className="features-items">
-                                                            <input type="radio" name="radio"/>
-                                                            <div className="feature-item-content">
-                                                                <div className="feature-item-title">01 Credit Plans</div>
-                                                                <span className="price-pc">$100</span>
-                                                            </div>
-                                                        </label>
+                                            {(isLoader)?<div className="loader" style={{textAlign:'center'}}><img src="assets/images/loader.svg"/></div>: plans.length>0 ?plans.map((data,index)=>{
+                                                return(
+                                                    <div className="col-12 col-md-6 col-lg-6" key={index}>
+                                                        <div className="radio-item-features">
+                                                            <label className="features-items">
+                                                                <input type="radio" name="radio"/>
+                                                                <div className="feature-item-content">
+                                                                    <div className="feature-item-title">{data.credit} Credit Plans</div>
+                                                                    <span className="price-pc">${data.price}</span>
+                                                                </div>
+                                                            </label>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div className="col-12 col-md-6 col-lg-6">
-                                                    <div className="radio-item-features">
-                                                        <label className="features-items">
-                                                            <input type="radio" name="radio"/>
-                                                            <div className="feature-item-content">
-                                                                <div className="feature-item-title">10 Credit Plans</div>
-                                                                <span className="price-pc">$1000</span>
-                                                            </div>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <div className="col-12 col-md-6 col-lg-6">
-                                                    <div className="radio-item-features">
-                                                        <label className="features-items">
-                                                            <input type="radio" name="radio"/>
-                                                            <div className="feature-item-content">
-                                                                <div className="feature-item-title">1,000 Credit Plans</div>
-                                                                <span className="price-pc">$100</span>
-                                                            </div>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <div className="col-12 col-md-6 col-lg-6">
-                                                    <div className="radio-item-features">
-                                                        <label className="features-items">
-                                                            <input type="radio" name="radio"/>
-                                                            <div className="feature-item-content">
-                                                                <div className="feature-item-title">100 Credit Plans</div>
-                                                                <span className="price-pc">$1000</span>
-                                                            </div>
-                                                        </label>
-                                                    </div>
-                                                </div>
+                                                )
+                                            }):''}
+
                                                 <div className="col-12 col-lg-12">
                                                     <div className="buynow-btn">
                                                         <a href="" className="btn btn-fill">Buy Now!</a>
--- src/component/pages/ChooseYourPlan.js
+++ src/component/pages/ChooseYourPlan.js
@@ -1,17 +1,65 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import Header from "../partials/Layouts/Header";
 import Footer from "../partials/Layouts/Footer";
+import {useAuth} from "../../hooks/useAuth"; 
 import { Link } from 'react-router-dom';
+import { toast } from "react-toastify";
+import axios from 'axios';
+
  const ChooseYourPlan = () => {
+    const [plans,setPlans] = useState([]);
+    const [isLoader, setIsLoader] = useState(true);
+    const [radioValue, setRadioValue] = useState('');
+    const [errorMsg,setErrorsMsg] = useState('');
+    const {getTokenData} = useAuth();
+    const apiUrl = process.env.REACT_APP_API_URL;
+
+    useEffect(()=>{
+        getPlans();
+    },[]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(radioValue,'radioValuerrr')
+        if(radioValue ==''){
+            setErrorsMsg('Please choose any plan');
+        }else{
+            setErrorsMsg('');
+        }
+    }
+    const getPlans = async ()=>{
+        try{
+            setIsLoader(true);
+            let headers = {
+                "Accept": "application/json", 
+                'Authorization': 'Bearer ' + getTokenData().access_token,
+                'auth-token' : getTokenData().access_token,
+            }
+
+            let plans = await axios.get(`${apiUrl}getPlans`, { headers: headers });
+            setPlans(plans.data.data.plans);
+            setIsLoader(false);
+        }catch(error){
+            if(error.response) {
+                if (error.response.data.errors) {
+                    toast.error(error.response.data.errors, {position: toast.POSITION.TOP_RIGHT});
+                }
+                if (error.response.data.error) {
+                    toast.error(error.response.data.error, {position: toast.POSITION.TOP_RIGHT});
+                }
+            }
+        }
+    }
+    console.log(errorMsg,'plans',radioValue);
     return(
         <>
             <Header/>
-            <section class="main-section position-relative pt-4 pb-5">
-                <div class="container position-relative">
-                    <div class="back-block">
-                        <div class="row">
-                            <div class="col-12 col-lg-4">
-                                <Link to="/" class="back">
+            <section className="main-section position-relative pt-4 pb-5">
+                <div className="container position-relative">
+                    <div className="back-block">
+                        <div className="row">
+                            <div className="col-12 col-lg-4">
+                                <Link to="/" className="back">
                                     <svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M15 6H1" stroke="#0A2540" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                                         <path d="M5.9 11L1 6L5.9 1" stroke="#0A2540" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
@@ -19,55 +67,69 @@ import { Link } from 'react-router-dom';
                                     Back
                                 </Link>
                             </div>
-                            <div class="col-12 col-lg-4">
-                                <h6 class="center-head text-center mb-0">Pricing Page</h6>
+                            <div className="col-12 col-lg-4">
+                                <h6 className="center-head text-center mb-0">Pricing Page</h6>
                             </div>
-                            <div class="col-12 col-lg-4"></div>
+                            <div className="col-12 col-lg-4"></div>
                         </div>
                     </div>
-                    <div class="card-box">
-                        <div class="row">
-                            <div class="col-12 col-lg-12">
-                                <div class="card-box-inner">
-                                    <div class="inner-page-title text-center">
-                                        <h3 class="text-center">Choose Your Plan</h3>
-                                        <p class="mb-0">You can purchase it accordingly Monthly, yearly or require more data then here is another plan for you.</p>
+                    <div className="card-box">
+                        <div className="row">
+                            <div className="col-12 col-lg-12">
+                                <div className="card-box-inner">
+                                    <div className="inner-page-title text-center">
+                                        <h3 className="text-center">Choose Your Plan</h3>
+                                        <p className="mb-0">You can purchase it accordingly Monthly, yearly or require more data then here is another plan for you.</p>
                                     </div>
-                                    <div class="card-box-light">
-                                        <form>
-                                            <div class="row">
-                                                <div class="col-12 col-lg-6">
-                                                    <div class="radio-item-features">
-                                                        <div class="package-plan">Monthly</div>
-                                                        <label class="features-items">
-                                                            <input type="radio" name="radio"/>
-                                                            <div class="feature-item-content">
-                                                                <div class="feature-item-title">40 Credit Plans<span class="small-text"> /yearly</span></div>
-                                                                <span class="price-pc">$100</span>
+                                    <div className="card-box-light">
+                                        {(isLoader)?<div className="loader" style={{textAlign:'center'}}><img src="assets/images/loader.svg"/></div>:
+                                        <form method="post" onSubmit={handleSubmit}>
+                                            <div className="row">
+                                                {plans.length >0 ? 
+                                                    plans.map((data,index)=>{
+                                                    return(
+                                                        <div className="row" key={index}>
+                                                            <div className="col-12 col-lg-6">
+                                                                <div className="radio-item-features">
+                                                                    <div className="package-plan">Monthly</div>
+                                                                    <label className="features-items">
+                                                                        <input type="radio" name="radio" onChange={(e)=>setRadioValue({id:data.id,type:'monthly'})}/>
+                                                                        <div className="feature-item-content">
+                                                                            <div className="feature-item-title">{data.monthly_credit} Credit Plans<span className="small-text"> /yearly</span></div>
+                                                                            <span className="price-pc">${data.month_amount}</span>
+                                                                        </div>
+                                                                    </label>
+                                                                </div>
                                                             </div>
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-12 col-lg-6">
-                                                    <div class="radio-item-features">
-                                                        <div class="package-plan">Yearly</div>
-                                                        <label class="features-items">
-                                                            <input type="radio" name="radio"/>
-                                                            <div class="feature-item-content">
-                                                                <div class="feature-item-title">40 Credit<span class="small-text">/Monthly</span></div>
-                                                                <span class="price-pc">$1000</span>
+                                                            <div className="col-12 col-lg-6">
+                                                                <div className="radio-item-features">
+                                                                    <div className="package-plan">Yearly</div>
+                                                                    <label className="features-items">
+                                                                        <input type="radio" name="radio"
+                                                                        onChange={(e)=>setRadioValue({id:data.id,type:'yearly'})}
+                                                                        />
+                                                                        <div className="feature-item-content">
+                                                                            <div className="feature-item-title">${data.monthly_credit} Credit<span className="small-text">/Monthly</span></div>
+                                                                            <span className="price-pc">${data.year_amount}</span>
+                                                                        </div>
+                                                                    </label>
+                                                                </div>
                                                             </div>
-                                                        </label>
+                                                        </div>
+                                                    )
+                                                }): ''}
+
+                                                <div className="col-12 col-lg-12">
+                                                    <p style={{textAlign: 'center',fontSize: '15px',color:'#ff0000'}}>{errorMsg}</p>
+                                                    <div className="buynow-btn">
+                                                        <button className="btn btn-fill">
+                                                            Buy Now!
+                                                        </button>
                                                     </div>
-                                                </div>
-                                                <div class="col-12 col-lg-12">
-                                                    <div class="buynow-btn">
-                                                        <a href="" class="btn btn-fill">Buy Now!</a>
-                                                    </div>
-                                                    <div class="note">Note : 1 Credit is 1 Buyer</div>
+                                                    <div className="note">Note : 1 Credit is 1 Buyer</div>
                                                 </div>
                                             </div>
-                                        </form>
+                                        </form>}
                                     </div>
                                 </div>
                             </div>
