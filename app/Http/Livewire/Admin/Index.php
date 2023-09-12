113fe001a12711b7f78d9f08f32ea202bfe8eb19
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2023-09-12 15:50:14 +0530

    make dynamic recent buyers

--- app/Http/Livewire/Admin/Index.php
+++ app/Http/Livewire/Admin/Index.php
@@ -20,9 +20,6 @@ class Index extends Component
 
         $purchasedBuyers = PurchasedBuyer::select('user_id','buyer_id', \DB::raw('MAX(created_at) as max_created_at'))->with(['user','buyer'])->where('user_id','!=',1)->groupBy('buyer_id')->orderBy('max_created_at', 'desc')->get();
 
-        // $purchasedBuyers = PurchasedBuyer::where('user_id','!=',1)->orderBy('created_at', 'desc')->take(5)->get();
-
-        // dd($purchasedBuyers);
         return view('livewire.admin.index', compact('buyerCount', 'sellerCount','purchasedBuyers'));
     }
 }
