367736a691b8435c2400a5b49a3d99f1d60ca5fd
amitpandey24 <amitpandey.his@gmail.com>
2023-11-20 15:36:02 +0530

    update code

--- src/component/pages/Buyers/BoostYourProfile.js
+++ src/component/pages/Buyers/BoostYourProfile.js
@@ -3,12 +3,14 @@ import BuyerHeader from "../../partials/Layouts/BuyerHeader";
 import { useAuth } from "../../../hooks/useAuth";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
-
+import { toast } from "react-toastify";
 import Footer from "../../partials/Layouts/Footer";
+import Swal from "sweetalert2";
 
 const BoostYourProfile = () => {
   const { getTokenData, setLogout } = useAuth();
   const [plans, setPlans] = useState("");
+  const [loader, setLoader] = useState(true);
   const fetchBuyerPlans = async () => {
     try {
       const apiUrl = process.env.REACT_APP_API_URL;
@@ -20,95 +22,259 @@ const BoostYourProfile = () => {
       let response = await axios.get(`${apiUrl}get-buyer-plans`, {
         headers: headers,
       });
-      console.log(response.data.buyer_plans);
+      setLoader(false);
       setPlans(response.data.buyer_plans);
-    } catch (error) {}
+    } catch (error) {
+      setLoader(false);
+      if (error.response) {
+        if (error.response.status === 401) {
+          setLogout();
+          //navigate('/login');
+        }
+        if (error.response.data.error) {
+          toast.error(error.response.data.error, {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        }
+      }
+    }
   };
   useEffect(() => {
     fetchBuyerPlans();
   }, []);
+  const boostProfilePurchase = async (planId, payment_type) => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL;
+      let headers = {
+        Accept: "application/json",
+        Authorization: "Bearer " + getTokenData().access_token,
+      };
+      let data = {
+        plan: planId,
+        type: payment_type,
+      };
+      let response = await axios.post(`${apiUrl}checkout-session`, data, {
+        headers: headers,
+      });
+      if (response.data.session) {
+        window.location.href = response.data.session.url;
+      }
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          setLogout();
+          //navigate('/login');
+        }
+        if (error.response.data.error) {
+          toast.error(error.response.data.error, {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        }
+      }
+    }
+  };
+
+  const handleChangeAutoPayment = async (e, planId) => {
+    try {
+      let is_plan_auto_renew = e.target.checked ? 1 : 0;
+      const apiUrl = process.env.REACT_APP_API_URL;
+      const data = { is_plan_auto_renew: is_plan_auto_renew, plan: planId };
+      let headers = {
+        Accept: "application/json",
+        Authorization: "Bearer " + getTokenData().access_token,
+      };
+      let title = "Do you want to make auto renew payment?";
+      let html = "It will auto deduct your payment";
+      if (!is_plan_auto_renew) {
+        title = "Do you want to stop auto renew payment?";
+        html = "You need to make payment manually next month";
+      }
+      Swal.fire({
+        icon: "warning",
+        title: title,
+        html: html,
+        showCancelButton: true,
+        confirmButtonText: "Yes",
+      }).then(async (result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          let response = await axios.post(
+            `${apiUrl}update-auto-renew-flag`,
+            data,
+            {
+              headers: headers,
+            }
+          );
+          if (response.data.status) {
+            toast.success(response.data.message, {
+              position: toast.POSITION.TOP_RIGHT,
+            });
+          }
+        } else {
+          const checkbox = document.getElementById("flexSwitchCheckChecked");
+          checkbox.checked = is_plan_auto_renew ? 0 : 1;
+        }
+      });
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status === 401) {
+          setLogout();
+          //navigate('/login');
+        }
+        if (error.response.data.error) {
+          toast.error(error.response.data.error, {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+        }
+      }
+    }
+  };
   return (
     <>
       <BuyerHeader />
       <section className="main-section position-relative pt-4 pb-120">
-        <div className="container position-relative">
-          <div className="back-block">
-            <div className="row">
-              <div className="col-4 col-sm-4 col-md-4 col-lg-4">
-                <Link to="/" className="back">
-                  <svg
-                    width="16"
-                    height="12"
-                    viewBox="0 0 16 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M15 6H1"
-                      stroke="#0A2540"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                    <path
-                      d="M5.9 11L1 6L5.9 1"
-                      stroke="#0A2540"
-                      strokeWidth="1.5"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    />
-                  </svg>
-                  Back
-                </Link>
+        {loader ? (
+          <div className="loader" style={{ textAlign: "center" }}>
+            <img src="assets/images/loader.svg" />
+          </div>
+        ) : (
+          <div className="container position-relative">
+            <div className="back-block">
+              <div className="row">
+                <div className="col-4 col-sm-4 col-md-4 col-lg-4">
+                  <Link to="/" className="back">
+                    <svg
+                      width="16"
+                      height="12"
+                      viewBox="0 0 16 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M15 6H1"
+                        stroke="#0A2540"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                      <path
+                        d="M5.9 11L1 6L5.9 1"
+                        stroke="#0A2540"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </svg>
+                    Back
+                  </Link>
+                </div>
+                <div className="col-7 col-sm-4 col-md-4 col-lg-4 align-self-center">
+                  <h6 className="center-head text-center mb-0">
+                    boost your profile
+                  </h6>
+                </div>
               </div>
-              <div className="col-7 col-sm-4 col-md-4 col-lg-4 align-self-center">
-                <h6 className="center-head text-center mb-0">
-                  boost your profile
-                </h6>
+            </div>
+            <div className="card-box boost-box">
+              <h3 className="text-center">Boost Your Profile</h3>
+              <p className="text-center">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry.
+              </p>
+              <div className="card-box-inner mt-4">
+                {plans.length > 0
+                  ? plans.map((data, index) => {
+                      return (
+                        <div className="boost-box-block-item" key={index}>
+                          <div className="boost-headingwithicon">
+                            <div className="boost-icon">
+                              <img src={data.image_url} />
+                            </div>
+                            <div className="boost-title">
+                              {data.title.toUpperCase()}
+                            </div>
+                          </div>
+                          <div className="postion">
+                            position : {data.position}
+                          </div>
+                          <div className="priceby">
+                            $ {data.amount} <span>/{data.type}</span>
+                          </div>
+                          <div className="notify">
+                            <img src="../assets/images/info.svg" />
+                            <div className="notifyinfo">{data.description}</div>
+                          </div>
+                          <div className="purchase-btn p-0">
+                            {!data.current_plan ? (
+                              <button
+                                type="button"
+                                className="btn btn-fill"
+                                onClick={() =>
+                                  boostProfilePurchase(
+                                    data.plan_stripe_id,
+                                    "boost_your_profile"
+                                  )
+                                }
+                              >
+                                purchase
+                              </button>
+                            ) : (
+                              <div className="renew-wrapper">
+                                <div className="renew-btnbox">
+                                  {data.current_plan ? (
+                                    <span className="badge bg-success purchased-profile">
+                                      Purchased
+                                    </span>
+                                  ) : (
+                                    <button
+                                      type="button"
+                                      className="btn btn-fill"
+                                      onClick={() =>
+                                        boostProfilePurchase(
+                                          data.plan_stripe_id,
+                                          "renew_buyer_profile"
+                                        )
+                                      }
+                                    >
+                                      Renew
+                                    </button>
+                                  )}
+                                </div>
+                                <div className="auto-renew">
+                                  Auto-Renew
+                                  <div className="form-check form-switch">
+                                    <input
+                                      className="form-check-input"
+                                      type="checkbox"
+                                      role="switch"
+                                      id="flexSwitchCheckChecked"
+                                      onChange={(e) =>
+                                        handleChangeAutoPayment(
+                                          e,
+                                          data.plan_stripe_id
+                                        )
+                                      }
+                                      defaultChecked={
+                                        data.is_plan_auto_renew ? "checked" : ""
+                                      }
+                                    />
+                                    <label
+                                      className="form-check-label"
+                                      htmlFor="flexSwitchCheckChecked"
+                                    ></label>
+                                  </div>
+                                </div>
+                              </div>
+                            )}
+                          </div>
+                        </div>
+                      );
+                    })
+                  : ""}
               </div>
             </div>
           </div>
-          <div className="card-box boost-box">
-            <h3 className="text-center">Boost Your Profile</h3>
-            <p className="text-center">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
-            <div className="card-box-inner mt-4">
-              {plans.length > 0
-                ? plans.map((data, index) => {
-                    return (
-                      <div className="boost-box-block-item">
-                        <div className="boost-headingwithicon">
-                          <div className="boost-icon">
-                            <img src={data.image_url} />
-                          </div>
-                          <div className="boost-title">
-                            {data.title.toUpperCase()}
-                          </div>
-                        </div>
-                        <div className="postion">
-                          position : {data.position}
-                        </div>
-                        <div className="priceby">
-                          $ {data.amount} <span>/{data.type}</span>
-                        </div>
-                        <div className="notify">
-                          <img src="../assets/images/info.svg" />
-                          <div className="notifyinfo">{data.description}</div>
-                        </div>
-                        <div className="purchase-btn">
-                          <button type="button" className="btn btn-fill">
-                            purchase
-                          </button>
-                        </div>
-                      </div>
-                    );
-                  })
-                : ""}
-            </div>
-          </div>
-        </div>
+        )}
       </section>
       <Footer />
     </>
